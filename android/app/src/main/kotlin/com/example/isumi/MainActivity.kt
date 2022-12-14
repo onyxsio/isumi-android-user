271ab900683cb66e5350cb30eb1091989b2f452e
Sudesh Nishshanka Bandara <sudeshnbandara@gmail.com>
2022-12-14 11:53:09 +0530

    version 1.1

--- android/app/src/main/kotlin/com/example/isumi/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/isumi/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.isumi.user
 
-import io.flutter.embedding.android.FlutterActivity
+// import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+// class MainActivity: FlutterActivity() {
+// }
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity : FlutterFragmentActivity() {}
